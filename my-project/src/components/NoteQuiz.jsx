10867296417b1a348f5a26f3cb318b98cf4745a3
Jonas Hultberg <jonas.hultberg@iths.se>
2024-06-08 21:21:23 +0200

    ready to send in

--- my-project/src/components/NoteQuiz.jsx
+++ my-project/src/components/NoteQuiz.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import Answer from "./Answer";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 const NoteQuiz = ({
   correct,
